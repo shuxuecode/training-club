e2c1e98b00123bf0ed08374ca239a7c80de38768
zhaoshuxue <zhaoshuxue@163.com>
2023-09-29 21:53:55 +0800

    :rewind: :arrow_down: :sparkles:

---
+++ project/springboot2716/src/main/java/com/springboot2716/springboot2716/Springboot2716Application.java
@@ -0,0 +1,13 @@
+package com.springboot2716.springboot2716;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2716Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2716Application.class, args);
+	}
+
+}
---
+++ project/springboot2716/src/test/java/com/springboot2716/springboot2716/Springboot2716ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot2716.springboot2716;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2716ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
